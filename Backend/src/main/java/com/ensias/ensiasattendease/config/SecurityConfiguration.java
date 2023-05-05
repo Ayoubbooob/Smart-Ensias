32b19fa5859163537b4a7a921bbd082e32ebaa7b
ayoubyte <ayoubboubkrioui2@gmail.com>
2023-05-05 19:56:03 +0100

    fix origin problem to let frontend communicate with backend

--- Backend/src/main/java/com/ensias/ensiasattendease/config/SecurityConfiguration.java
+++ Backend/src/main/java/com/ensias/ensiasattendease/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.ensias.ensiasattendease.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -23,11 +26,16 @@ public class SecurityConfiguration {
     private final LogoutHandler logoutHandler;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.cors().disable();
         httpSecurity
                 .csrf()
                 .disable()
+                .cors().and() // added
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**") //ADD HERE ENDPOINTS THAT YOU DON'T NEED TO AUTHENTICATE TO USE THEM(WHITELIST)
+                .requestMatchers("/api/v1/auth/**")//ADD HERE ENDPOINTS THAT YOU DON'T NEED TO AUTHENTICATE TO USE THEM(WHITELIST)
+//
+//                .requestMatchers("/**")//ADD HERE ENDPOINTS THAT YOU DON'T NEED TO AUTHENTICATE TO USE THEM(WHITELIST)
+
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -43,4 +51,7 @@ public class SecurityConfiguration {
                 .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext());
         return httpSecurity.build();
     }
+
+
+
 }
