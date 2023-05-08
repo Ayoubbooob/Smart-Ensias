6e24119df3cc9d73c62e378a1af1de0e8d526a7e
ayoubyte <ayoubboubkrioui2@gmail.com>
2023-05-08 22:19:57 +0100

    add all Event functionnality , CRUD

--- Backend/src/main/java/com/ensias/ensiasattendease/controllers/EventController.java
+++ Backend/src/main/java/com/ensias/ensiasattendease/controllers/EventController.java
@@ -5,10 +5,34 @@ import com.ensias.ensiasattendease.models.EventModel;
 import com.ensias.ensiasattendease.resources.RequestModels.AddEventRequest;
 import com.ensias.ensiasattendease.services.implementations.EventServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
+//@RestController
+//@RequestMapping("api/v1/event")
+//@RequiredArgsConstructor
+//public class EventController {
+//
+//    private final EventServiceImpl eventService;
+//    @PostMapping("/create")
+//    public ResponseEntity<EventModel> createEvent(@RequestBody AddEventRequest addEventRequest){
+//        return ResponseEntity.ok(eventService.createEvent(addEventRequest));
+//    }
+//
+//    @GetMapping("/all")
+//    public ResponseEntity<List<EventModel>> getAllEvents(){
+//        return ResponseEntity.ok(eventService.getAllEvents());
+//    }
+//
+//
+//
+//}
+
 
 @RestController
 @RequestMapping("api/v1/event")
@@ -16,16 +40,69 @@ import java.util.List;
 public class EventController {
 
     private final EventServiceImpl eventService;
+
     @PostMapping("/create")
-    public ResponseEntity<EventModel> createEvent(@RequestBody AddEventRequest addEventRequest){
-        return ResponseEntity.ok(eventService.createEvent(addEventRequest));
+    public ResponseEntity<EventModel> createEvent(@RequestBody AddEventRequest addEventRequest) throws Exception {
+        try {
+            EventModel event = eventService.createEvent(addEventRequest);
+            return ResponseEntity.ok(event);
+        } catch (Exception e) {
+            throw new Exception("Failed to create event: " + e.getMessage());
+        }
     }
 
     @GetMapping("/all")
-    public ResponseEntity<List<EventModel>> getAllEvents(){
-        return ResponseEntity.ok(eventService.getAllEvents());
+    public ResponseEntity<List<EventModel>> getAllEvents() throws Exception {
+        try {
+            List<EventModel> events = eventService.getAllEvents();
+            return ResponseEntity.ok(events);
+        } catch (Exception e) {
+            throw new Exception("Failed to get events: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventModel> getEventById(@PathVariable Long eventId) throws Exception {
+        try {
+            EventModel event = eventService.getEventById(eventId);
+            if(event == null){
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(event);
+        } catch (Exception e) {
+            throw new Exception("Failed to get event: " + e.getMessage());
+        }
+    }
+
+    @PutMapping("/{eventId}")
+    public ResponseEntity<EventModel> updateEvent(@PathVariable Long eventId, @RequestBody AddEventRequest addEventRequest) throws Exception {
+        try {
+            EventModel updatedEvent = eventService.updateEvent(eventId, addEventRequest);
+            return ResponseEntity.ok(updatedEvent);
+        } catch (Exception e) {
+            throw new Exception("Failed to update event: " + e.getMessage());
+        }
+    }
+
+    @PatchMapping("/{eventId}")
+    public ResponseEntity<EventModel> patchEvent(@PathVariable Long eventId, @RequestBody Map<String, Object> updates) throws Exception {
+        try {
+            EventModel patchedEvent = eventService.patchEvent(eventId, updates);
+            return ResponseEntity.ok(patchedEvent);
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();        }
+    }
+
+    @DeleteMapping("/{eventId}")
+    public ResponseEntity<Void> deleteEvent(@PathVariable Long eventId) throws Exception {
+        try {
+            eventService.deleteEvent(eventId);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
-
 }
+
--- Backend/src/main/java/com/ensias/ensiasattendease/services/implementations/EventServiceImpl.java
+++ Backend/src/main/java/com/ensias/ensiasattendease/services/implementations/EventServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 
 @Service
@@ -28,7 +30,59 @@ public class EventServiceImpl implements EventService {
         return eventToSave;
     }
 
+
+
+    public EventModel getEventById(Long eventId) {
+        return eventRepository.findById(eventId).orElse(null);
+    }
+
     public List<EventModel> getAllEvents() {
         return eventRepository.findAll();
+    }
+
+
+    public EventModel updateEvent(Long eventId, AddEventRequest addEventRequest) throws Exception {
+        Optional<EventModel> optionalEvent = eventRepository.findById(eventId);
+        if (optionalEvent.isEmpty()) {
+            throw new Exception("Event not found with id: " + eventId);
+        }
+        EventModel existingEvent = optionalEvent.get();
+        existingEvent.setStart_date(LocalDate.parse(addEventRequest.getStart_date()));
+        existingEvent.setEnd_date(LocalDate.parse(addEventRequest.getEnd_date()));
+        existingEvent.setTitle(addEventRequest.getTitle());
+        eventRepository.save(existingEvent);
+        return existingEvent;
+    }
+
+    public EventModel patchEvent(Long eventId, Map<String, Object> updates) throws Exception {
+        Optional<EventModel> optionalEvent = eventRepository.findById(eventId);
+        if (optionalEvent.isEmpty()) {
+            throw new Exception("Event not found with id: " + eventId);
+        }
+        EventModel existingEvent = optionalEvent.get();
+
+        for (Map.Entry<String, Object> entry : updates.entrySet()) {
+            String field = entry.getKey();
+            Object value = entry.getValue();
+            switch (field) {
+                case "start_date" -> existingEvent.setStart_date(LocalDate.parse(value.toString()));
+                case "end_date" -> existingEvent.setEnd_date(LocalDate.parse(value.toString()));
+                case "title" -> existingEvent.setTitle(value.toString());
+                default -> throw new IllegalArgumentException("Invalid field: " + field);
+            }
+        }
+
+        eventRepository.save(existingEvent);
+        return existingEvent;
+    }
+
+
+    public void deleteEvent(Long eventId) throws Exception {
+        Optional<EventModel> event = eventRepository.findById(eventId);
+        if (event.isPresent()) {
+            eventRepository.delete(event.get());
+        } else {
+            throw new Exception("Event with ID " + eventId + " not found");
+        }
     }
 }
