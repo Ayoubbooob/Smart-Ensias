1758eb1ed0e593b7054aacae2cbae43b0a6998c2
ayoubyte <ayoubboubkrioui2@gmail.com>
2023-05-09 01:44:46 +0100

    add Teacher & Holiday features

--- Backend/src/main/java/com/ensias/ensiasattendease/services/HolidayService.java
+++ Backend/src/main/java/com/ensias/ensiasattendease/services/HolidayService.java
@@ -3,7 +3,22 @@ package com.ensias.ensiasattendease.services;
 import com.ensias.ensiasattendease.models.HolidayModel;
 import com.ensias.ensiasattendease.resources.RequestModels.AddHolidayRequest;
 
+import java.util.List;
+import java.util.Map;
+
 public interface HolidayService {
 
-    public HolidayModel createHoliday(AddHolidayRequest addHolidayRequest);
+     HolidayModel createHoliday(AddHolidayRequest addHolidayRequest);
+
+     HolidayModel getHolidayById(Long holidayId);
+
+    List<HolidayModel> getAllHolidays();
+
+    HolidayModel patchHoliday(Long holidayId, Map<String, Object> updates) throws Exception;
+
+    HolidayModel updateHoliday(Long holidayId, AddHolidayRequest addHolidayRequest) throws Exception;
+
+    public void deleteHoliday(Long holidayId) throws Exception;
+
+
 }
